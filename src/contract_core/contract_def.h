973f861006164d157e1c6489877ca562d2610452
double-k-3033 <double-k-3033@users.noreply.github.com>
2026-06-02 20:37:32 +0900

    feat: qfomo skeleton

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -288,6 +288,16 @@
 #define CONTRACT_STATE2_TYPE ESCROW2
 #include "contracts/Escrow.h"
 
+#undef CONTRACT_INDEX
+#undef CONTRACT_STATE_TYPE
+#undef CONTRACT_STATE2_TYPE
+
+#define QFOMO_CONTRACT_INDEX 27
+#define CONTRACT_INDEX QFOMO_CONTRACT_INDEX
+#define CONTRACT_STATE_TYPE QFOMO
+#define CONTRACT_STATE2_TYPE QFOMO2
+#include "contracts/QFomo.h"
+
 // new contracts should be added above this line
 
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
@@ -402,6 +412,7 @@ constexpr struct ContractDescription
     {"VOTTUN", 206, 10000, sizeof(VOTTUNBRIDGE::StateData)}, // proposal in epoch 204, IPO in 205, construction and first use in 206
     {"QUSINO", 208, 10000, sizeof(QUSINO::StateData)}, // proposal in epoch 206, IPO in 207, construction and first use in 208
     {"ESCROW", 210, 10000, sizeof(ESCROW::StateData)}, // proposal in epoch 208, IPO in 209, construction and first use in 210
+    {"QFOMO", 215, 10000, sizeof(QFOMO::StateData)}, // proposal in epoch 213, IPO in 214, construction and first use in 210
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(TESTEXA::StateData)},
@@ -525,6 +536,7 @@ static void initializeContracts()
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(VOTTUNBRIDGE);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QUSINO);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(ESCROW);
+    REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QFOMO);
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(TESTEXA);
---
+++ src/contracts/QFomo.h
@@ -0,0 +1,28 @@
+using namespace QPI;
+#include "qpi.h"
+
+struct QFOMO2
+{
+};
+
+struct QFOMO : public ContractBase
+{
+    struct StateData
+    {
+    };
+
+    REGISTER_USER_FUNCTIONS_AND_PROCEDURES()
+    {
+
+    }
+    
+    INITIALIZE()
+    {
+
+    }
+
+    END_EPOCH()
+    {
+
+    } 
+};
